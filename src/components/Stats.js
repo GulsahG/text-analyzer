b5aa18814b392cd0be3377572ef91ce9ffa628ac
Gülşah Genç <gulsahgenc.gg@gmail.com>
2021-03-08 13:15:54 +0300

    Functions commented out

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,26 +3,37 @@ import React from 'react';
 const readingTime = require('reading-time');
 
 class Stats extends React.Component {
+  // returns the amount of words in the current prop text
   wordCount = () => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
+    // creates an arr of words
     const splitText = text.trim().split('');
+    // returns 0 if there are no words
     if(!splitText.length)
       return 0;
-
+    
     const spaces = splitText.filter(char => char === ' ');
     return (this.props.text.length > 0 && spaces.length === 0) ? 1 : spaces.length+1;
   }
-  
+
+  // returns the amount of letters in the current prop text
   numOfLetters = () => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
     const splitText = text.trim().split(' ');
     return splitText.join('').length;
   }
 
+  // returns the word with the longest length in the current prop text
   longestWord = () => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
     const splitText = text.trim().split(' ');
     let longest = 0, longestWord = '🤷';
@@ -35,47 +46,65 @@ class Stats extends React.Component {
     return longestWord;
   }
 
+  // returns the average length of the words in the current prop text
   averageWordLength = () => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
     const splitText = text.trim().split(' ');
     const numOfChars = splitText.join('').length;
     return (numOfChars / this.wordCount() > 0) ? (numOfChars / this.wordCount()).toFixed(2) : 0;
   }
 
+  // returns the reading duration in seconds of the current prop text
   readingDuration = () => {
+    // replaces everything but letters and numbers with a space
     let text = this.props.text.replace(/[^A-Za-z0-9]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');;
     const stats = readingTime(text);
     return `${(stats.minutes * 60).toFixed(2) > 0 ? (stats.minutes * 60).toFixed(2) : 0} s`;
   }
 
+  // returns the length of the median word of the current prop text
   medianWordLength = () => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
     const splitText = text.trim().split(' ');
     const middle = Math.floor(splitText.length / 2);
     return splitText[middle].length;
   }
 
+  // returns the length of the median word,
+  // in the ascending order of length, of the current prop text
   sortedMedianWordLength = () => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
     const splitText = text.trim().split(' ');
     const middle = Math.floor(splitText.length / 2);
+    // sorts the arr in the ascending order
     const orderedText = splitText.sort((a,b) => a.length - b.length);
     return orderedText[middle].length;
   }
 
-  // the num of words needed can be implemented
+  // returns the top *num* common words in the current prop text
   topCommonWords = (num) => {
+    // replaces everything but letters with a space
     let text = this.props.text.replace(/[^A-Za-z]/g, ' ');
+    // gets rid of duplicate spaces
     text = text.replace( /  +/g, ' ');
     let splitText = text.trim().split(' ');
+    // adds a count value to each word
     splitText = splitText.map((word) => {
       return [word, 0];
     });
 
+    // updates the count of the first repeated occurrence of a word
     for(let i = 0; i < splitText.length; i++) {
       let currWord = splitText[i][0];
       for(let y = 0; y < splitText.length; y++) {
@@ -86,15 +115,19 @@ class Stats extends React.Component {
       }
     }
 
+    // sorts the arr in the descending order of the word count values
     let orderedText = splitText.sort((a,b) => b[1] - a[1]);
+    // gets rid of the duplicate words
     orderedText = orderedText.filter(word => word[1] !== 0);
     
+    // returns the needed top *num* common words
     let str = '', i = 0;
     while(i < num){
       if(orderedText[i])
         str += `${orderedText[i][0]}, `;
       i++;
     }
+    // sets a default value for when there is no words
     return str.match(/[A-Za-z]/g) ? str.slice(0, -2) : '🤷';
   }
 
